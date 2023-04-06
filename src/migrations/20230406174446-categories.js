8e0774e5305419be3cf6a1b4a0db6b8363ce09a8
Pedro <pedrohalmeidamendonca@gmail.com>
2023-04-06 15:28:38 -0300

    Requisito 01

---
+++ src/migrations/20230406174446-categories.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.createTable('categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    }),
+
+  down: async (queryInterface) => queryInterface.dropTable('categories'),
+};
