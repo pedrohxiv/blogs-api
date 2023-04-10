7506f06736ba63c9bec793b39b29a69cc44a0af3
Pedro <pedrohalmeidamendonca@gmail.com>
2023-04-10 18:07:33 -0300

    Requisito 07

---
+++ src/models/Category.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false,
+      tableName: 'categories',
+      underscored: true,
+    },
+  );
+
+  return Category;
+};
